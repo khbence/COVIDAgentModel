af4bc82f9b39e7d817fd9aaffefa94671de9f3f0
khbence <horvathb94@gmail.com>
2020-04-08 14:22:54 +0200

    singleton AgentList, deleted useless source files

--- include/agentsList.h
+++ include/agentsList.h
@@ -5,16 +5,21 @@ template<typename T>
 concept PPStateType = requires (T x) { x.update(); x.gotInfected(); };
 */
 
-//singleton
+// singleton
 
 template<typename PPState, typename Location>
 class AgentList {
     std::vector<PPState> PPValues;
     std::vector<bool> diagnosed;
     std::vector<Location*> localizations;
+
+    AgentList() = default;
+
 public:
+    [[nodiscard]] static AgentList* getInstance() {
+        static AgentList instance;
+        return &instance;
+    }
 
-    bool checkConsistency() const; //if all vector are of the same lengths
-
+    [[nodiscard]] bool checkConsistency() const;// if all vector are of the same lengths
 };
-
