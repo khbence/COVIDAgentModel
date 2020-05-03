014b0b047cbd4cef1046e6b8b0a62d66d86c6a42
Istvan Reguly <regulyistvan@gmail.com>
2020-05-03 21:17:10 +0000

    apparently transform_reduce does not let you modify. for_each does

--- include/location.h
+++ include/location.h
@@ -41,26 +41,19 @@ public:
         auto& ppstates = SimulationType::AgentListType::getInstance()->PPValues;
         thrust::device_vector<float> rnds(agents.size());
         rnds = RandomGenerator::fillUnitf(agents.size());
-        //auto &rnds_host 
-        //thrust::copy(rnds_host.begin(), rnds_host.end(), rnds.begin());
-
-        int newInfections = thrust::transform_reduce(
+        int inf1 = count_if(ppstates.begin(), ppstates.end(), [](auto &ppstate) {return ppstate.getSIRD() == states::SIRD::I;});
+        thrust::for_each(
             thrust::make_zip_iterator(thrust::make_tuple(rnds.begin(),
                 thrust::make_permutation_iterator(ppstates.begin(), agents.begin()))),
             thrust::make_zip_iterator(thrust::make_tuple(rnds.end(),
                 thrust::make_permutation_iterator(ppstates.begin(), agents.end()))),
-            [=](auto& i) {
+            [=](auto i) {
                 auto &rnd = thrust::get<0>(i);
                 auto &a = thrust::get<1>(i);
                 if (a.getSIRD() == states::SIRD::S && rnd < ratio) {
                     a.gotInfected();
-                    return 1;
                 }
-                return 0;
-            },
-            0,
-            thrust::plus<int>());
-        //if (newInfections) std::cout << newInfections << std::endl;
+            });
     }
 
     const auto& refreshAndGetStatistic() { return stat.refreshandGetAfterMidnight(agents); }
