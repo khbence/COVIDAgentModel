c338e5d039ce9ba162feb38db7cc14d6970919b9
khbence <horvathb94@gmail.com>
2020-06-23 14:24:02 +0200

    clean up

--- MATLAB/define_types_epidemic.m
+++
@@ -1,6 +0,0 @@
-function types=define_types_epidemic
-
-        types{1}=define_type_1_epid;
-   
-        %....
-end
--- MATLAB/determine_object_width_from_area.m
+++
@@ -1,5 +0,0 @@
-function y=determine_object_width_from_area(area)
-
-        y=min(area^(1/4)/10,1.9);
-        
-end
