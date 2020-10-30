b6bb3fb9a6650376269cf291f36ae5866edf709d
khbence <horvathb94@gmail.com>
2020-10-30 10:53:05 +0100

    hotfix

--- include/tools/smallTools.h
+++ include/tools/smallTools.h
@@ -3,7 +3,7 @@
 #include <random>
 #include <omp.h>
 #include <array>
-#include <exception>
+#include <stdexcept>
 
 std::string separator();
 
@@ -14,6 +14,11 @@ auto SecantMethod(lambda&& func, double x0, double x1, double precision) {
     while (precision < std::abs(func(x_k))) {
         auto temp = x_k;
         auto diff = func(x_k) - func(x_prev);
+        if (diff != 0.0) {
+            x_k -= func(x_k) * (x_k - x_prev) / diff;
+        } else {
+            throw std::runtime_error("\tSecant stucked\n");
+        }
         x_prev = temp;
         ++iterations;
     }
